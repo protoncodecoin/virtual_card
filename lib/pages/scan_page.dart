600ad7c8613a3ec2b1ae1bfb7af8aa443f031f38
Prince Asante <princeaffumasante@gmail.com>
2024-11-04 10:37:48 +0000

    capture or select image from gallery

---
+++ lib/pages/scan_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ScanPage extends StatefulWidget {
+  static const String routeName = "scan";
+  const ScanPage({super.key});
+
+  @override
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scan Contact Card"),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  getImage(ImageSource.camera);
+                },
+                icon: const Icon(Icons.camera),
+                label: const Text("Capture"),
+              ),
+              TextButton.icon(
+                onPressed: () {
+                  getImage(ImageSource.gallery);
+                },
+                icon: const Icon(Icons.photo_album),
+                label: const Text("Gallery"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void getImage(ImageSource camera) async {
+    final xFile = await ImagePicker().pickImage(source: camera);
+
+    if (xFile != null) {
+      print(xFile.path);
+    }
+  }
+}
