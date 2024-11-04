294ca091ffaebbee5b01dbf5c6b58400ceb461d7
Prince Asante <princeaffumasante@gmail.com>
2024-11-04 14:56:21 +0000

    build form page

--- lib/pages/form_page.dart
+++ lib/pages/form_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_card/models/contact_model.dart';
+import 'package:virtual_card/utils/constants.dart';
 
 class FormPage extends StatefulWidget {
   static const String routeName = 'form';
@@ -12,11 +13,67 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final _formkey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _mobieController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _companyController = TextEditingController();
+  final _webController = TextEditingController();
+  final _designationController = TextEditingController();
+
+  @override
+  void initState() {
+    _nameController.text = widget.contactModel.name;
+    _mobieController.text = widget.contactModel.mobile;
+    _addressController.text = widget.contactModel.address;
+    _emailController.text = widget.contactModel.email;
+    _companyController.text = widget.contactModel.company;
+    _webController.text = widget.contactModel.website;
+    _designationController.text = widget.contactModel.designation;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _mobieController.dispose();
+    _addressController.dispose();
+    _emailController.dispose();
+    _companyController.dispose();
+    _webController.dispose();
+    _designationController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.contactModel.name),
+        title: const Text("Form Page"),
+      ),
+      body: Form(
+        key: _formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: "Contact Name",
+                prefixIcon: Icon(Icons.person),
+                border: InputBorder.none,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return emptyFieldErrorMsg;
+                }
+                return null;
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,3 +10,4 @@ abstract final class ContactProperties {
 
 const String hint =
     'Long press and Drag each item from the below list and drop above in the appropriate box';
+const String emptyFieldErrorMsg = 'This field should not be empty';
