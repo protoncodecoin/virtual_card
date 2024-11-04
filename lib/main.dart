af5d7d030ac324c37721c725fece7bb6c12abdde
Prince Asante <princeaffumasante@gmail.com>
2024-11-04 09:39:44 +0000

    project setup create directory for app components configure route for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:virtual_card/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      routerConfig: _router,
       theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:Placeholder(),
     );
   }
+
+  // router
+  final _router = GoRouter(
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        name: HomePage.routeName,
+        path: HomePage.routeName,
+        builder: (context, state) => const HomePage(),
+      ),
+    ],
+  );
 }
