d1fdb0adfce713001ce7b46b1e88a647f6733c47
Prince Asante <princeaffumasante@gmail.com>
2024-11-06 11:10:41 +0000

    add method save contact to db

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:virtual_card/pages/scan_page.dart';
+import 'package:virtual_card/providers/contact_provider.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = "/";
@@ -12,6 +14,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int selectedIndex = 0;
+
+  @override
+  void didChangeDependencies() {
+    Provider.of<ContactProvider>(context, listen: false).getAllContacts();
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +54,22 @@ class _HomePageState extends State<HomePage> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.favorite), label: "Favorite"),
           ],
+        ),
+      ),
+      body: Consumer<ContactProvider>(
+        builder: (context, provider, child) => ListView.builder(
+          itemCount: provider.contactList.length,
+          itemBuilder: (context, index) {
+            final contact = provider.contactList[index];
+            return ListTile(
+              title: Text(contact.name),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                    contact.favorite ? Icons.favorite : Icons.favorite_border),
+              ),
+            );
+          },
         ),
       ),
     );
